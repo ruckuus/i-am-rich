093fd3a65a149f6932c15ea0bfa0e2bc5fc72028
Dwi S Supriyadi <ruckuus@gmail.com>
2021-01-30 17:10:56 +0800

    Bismillah. I Am Rich.

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Admin on 29/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
